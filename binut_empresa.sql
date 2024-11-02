be17f0d83c17dc5aee5794c9c8a696f5be01c753
maricasts <mariaimaculada7773@gmail.com>
2024-11-02 16:17:55 -0300

    banco_backup_save

---
+++ binut_empresa.sql
@@ -0,0 +1,269 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 02/11/2024 às 20:04
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `binut_empresa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `id_cliente` int(11) NOT NULL,
+  `nome_cliente` varchar(100) NOT NULL,
+  `email_cliente` varchar(100) NOT NULL,
+  `senha_cliente` varchar(255) NOT NULL,
+  `id_endereco` int(11) NOT NULL,
+  `data_cadastro` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `consulta`
+--
+
+CREATE TABLE `consulta` (
+  `id_consulta` int(11) NOT NULL,
+  `con_preco` decimal(10,2) NOT NULL,
+  `con_desc` text DEFAULT NULL,
+  `con_horario` datetime NOT NULL,
+  `id_nutri` int(11) NOT NULL,
+  `id_cliente` int(11) NOT NULL,
+  `status_consulta` enum('Agendada','Realizada','Cancelada') DEFAULT 'Agendada',
+  `data_criacao` timestamp NOT NULL DEFAULT current_timestamp()
+) ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `endereco`
+--
+
+CREATE TABLE `endereco` (
+  `id_endereco` int(11) NOT NULL,
+  `rua` varchar(100) NOT NULL,
+  `numero` varchar(10) NOT NULL,
+  `complemento` varchar(50) DEFAULT NULL,
+  `cidade` varchar(50) NOT NULL,
+  `bairro` varchar(50) NOT NULL,
+  `CEP` varchar(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Acionadores `endereco`
+--
+DELIMITER $$
+CREATE TRIGGER `verificar_cep` BEFORE INSERT ON `endereco` FOR EACH ROW BEGIN 
+    IF LENGTH(NEW.CEP) != 8 THEN 
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'O CEP deve ter exatamente 8 caracteres.'; 
+    END IF; 
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `especialidade`
+--
+
+CREATE TABLE `especialidade` (
+  `id_especialidade` bigint(20) UNSIGNED NOT NULL,
+  `descricao` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `nutricionista`
+--
+
+CREATE TABLE `nutricionista` (
+  `id_nutri` int(11) NOT NULL,
+  `nome_nutri` varchar(100) NOT NULL,
+  `email_nutri` varchar(225) NOT NULL,
+  `CRN_nutri` varchar(4) NOT NULL,
+  `senha_nutri` varchar(255) NOT NULL,
+  `id_endereco` int(11) NOT NULL,
+  `data_cadastro` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `nutricionista_especialidade`
+--
+
+CREATE TABLE `nutricionista_especialidade` (
+  `id_nutri` int(11) NOT NULL,
+  `id_especialidade` bigint(20) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `pagamento`
+--
+
+CREATE TABLE `pagamento` (
+  `id_pagamento` int(11) NOT NULL,
+  `id_consulta` int(11) NOT NULL,
+  `id_nutri` int(11) NOT NULL,
+  `id_cliente` int(11) NOT NULL,
+  `horario_pag` datetime NOT NULL DEFAULT current_timestamp(),
+  `valor_pago` decimal(10,2) NOT NULL,
+  `status_pagamento` enum('Pendente','Pago','Cancelado') DEFAULT 'Pendente'
+) ;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id_cliente`),
+  ADD UNIQUE KEY `uk_email_cliente` (`email_cliente`);
+
+--
+-- Índices de tabela `consulta`
+--
+ALTER TABLE `consulta`
+  ADD PRIMARY KEY (`id_consulta`),
+  ADD KEY `id_nutri` (`id_nutri`),
+  ADD KEY `id_cliente` (`id_cliente`);
+
+--
+-- Índices de tabela `endereco`
+--
+ALTER TABLE `endereco`
+  ADD PRIMARY KEY (`id_endereco`);
+
+--
+-- Índices de tabela `especialidade`
+--
+ALTER TABLE `especialidade`
+  ADD PRIMARY KEY (`id_especialidade`);
+
+--
+-- Índices de tabela `nutricionista`
+--
+ALTER TABLE `nutricionista`
+  ADD PRIMARY KEY (`id_nutri`),
+  ADD UNIQUE KEY `uk_email_nutri` (`email_nutri`),
+  ADD UNIQUE KEY `uk_crn` (`CRN_nutri`),
+  ADD KEY `id_endereco` (`id_endereco`);
+
+--
+-- Índices de tabela `nutricionista_especialidade`
+--
+ALTER TABLE `nutricionista_especialidade`
+  ADD PRIMARY KEY (`id_nutri`,`id_especialidade`),
+  ADD KEY `id_especialidade` (`id_especialidade`);
+
+--
+-- Índices de tabela `pagamento`
+--
+ALTER TABLE `pagamento`
+  ADD PRIMARY KEY (`id_pagamento`),
+  ADD KEY `id_consulta` (`id_consulta`),
+  ADD KEY `id_nutri` (`id_nutri`),
+  ADD KEY `id_cliente` (`id_cliente`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `consulta`
+--
+ALTER TABLE `consulta`
+  MODIFY `id_consulta` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `endereco`
+--
+ALTER TABLE `endereco`
+  MODIFY `id_endereco` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `especialidade`
+--
+ALTER TABLE `especialidade`
+  MODIFY `id_especialidade` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `nutricionista`
+--
+ALTER TABLE `nutricionista`
+  MODIFY `id_nutri` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `pagamento`
+--
+ALTER TABLE `pagamento`
+  MODIFY `id_pagamento` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `consulta`
+--
+ALTER TABLE `consulta`
+  ADD CONSTRAINT `consulta_ibfk_1` FOREIGN KEY (`id_nutri`) REFERENCES `nutricionista` (`id_nutri`),
+  ADD CONSTRAINT `consulta_ibfk_2` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id_cliente`);
+
+--
+-- Restrições para tabelas `nutricionista`
+--
+ALTER TABLE `nutricionista`
+  ADD CONSTRAINT `nutricionista_ibfk_1` FOREIGN KEY (`id_endereco`) REFERENCES `endereco` (`id_endereco`);
+
+--
+-- Restrições para tabelas `nutricionista_especialidade`
+--
+ALTER TABLE `nutricionista_especialidade`
+  ADD CONSTRAINT `nutricionista_especialidade_ibfk_1` FOREIGN KEY (`id_nutri`) REFERENCES `nutricionista` (`id_nutri`),
+  ADD CONSTRAINT `nutricionista_especialidade_ibfk_2` FOREIGN KEY (`id_especialidade`) REFERENCES `especialidade` (`id_especialidade`);
+
+--
+-- Restrições para tabelas `pagamento`
+--
+ALTER TABLE `pagamento`
+  ADD CONSTRAINT `pagamento_ibfk_1` FOREIGN KEY (`id_consulta`) REFERENCES `consulta` (`id_consulta`),
+  ADD CONSTRAINT `pagamento_ibfk_2` FOREIGN KEY (`id_nutri`) REFERENCES `nutricionista` (`id_nutri`),
+  ADD CONSTRAINT `pagamento_ibfk_3` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id_cliente`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
